e543624938107a7f65e09a6f391f323646cc6bba
Robin Zheng <zhengr@msn.com>
2023-06-21 12:32:35 +0800

    Add files via upload
    change  share=True
    
    (demo
     .queue(concurrency_count=3)
     .launch(server_name='0.0.0.0',
             server_port=7860,
             show_api=False,
             share=True,
             inbrowser=False))

--- configs/model_config.py
+++ configs/model_config.py
@@ -16,7 +16,7 @@ embedding_model_dict = {
     "ernie-tiny": "nghuyong/ernie-3.0-nano-zh",
     "ernie-base": "nghuyong/ernie-3.0-base-zh",
     "text2vec-base": "shibing624/text2vec-base-chinese",
-    "text2vec": "GanymedeNil/text2vec-large-chinese",
+    "text2vec": "/content/langchain-ChatGLM/text2vec-large-chinese",
     "m3e-small": "moka-ai/m3e-small",
     "m3e-base": "moka-ai/m3e-base",
 }
@@ -43,7 +43,7 @@ llm_model_dict = {
     "chatglm-6b-int4": {
         "name": "chatglm-6b-int4",
         "pretrained_model_name": "THUDM/chatglm-6b-int4",
-        "local_model_path": None,
+        "local_model_path": "/content/langchain-ChatGLM/chatglm-6b-int4",
         "provides": "ChatGLM"
     },
     "chatglm-6b-int8": {
@@ -98,7 +98,7 @@ llm_model_dict = {
 }
 
 # LLM 名称
-LLM_MODEL = "chatglm-6b"
+LLM_MODEL = "chatglm-6b-int4"
 # 量化加载8bit 模型
 LOAD_IN_8BIT = False
 # Load the model with bfloat16 precision. Requires NVIDIA Ampere GPU.
